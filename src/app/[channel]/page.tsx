b22be3939ca58ca3008753ad4d4e84515e4c9cc9
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-06 00:54:26 +0100

    feat: enhance ChatPage with improved emote handling and error management

--- src/app/[channel]/page.tsx
+++ src/app/[channel]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState, useRef, JSX } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
 
@@ -10,8 +10,11 @@ import { SevenTV } from '@/lib/7TV';
 import { Emote } from '@/lib/Emote';
 import { handleCommand } from '@/lib/Command';
 
+import { replaceEmotes } from '@/utils/emoteUtils';
+
 import type { Message } from '@/types/Chat';
 import type { STVEmote, TwitchEmoteData } from '@/types/Emote';
+import { adjustColorBrightness } from '@/utils/colorUtils';
 
 const MAX_MESSAGES = 20;
 
@@ -21,7 +24,10 @@ export default function ChatPage() {
 	const [messages, setMessages] = useState<Message[]>([]);
 	const messagesEndRef = useRef<HTMLDivElement>(null);
 	const [emotes, setEmotes] = useState<TwitchEmoteData | null>(null);
-	const [stvGlobalEmotes, setSTVGlobalEmotes] = useState<STVEmote[] | null>(
+	const [STVGlobalEmotes, setSTVGlobalEmotes] = useState<STVEmote[] | null>(
+		null
+	);
+	const [STVChannelEmotes, setSTVChannelEmotes] = useState<STVEmote[] | null>(
 		null
 	);
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -38,37 +44,50 @@ export default function ChatPage() {
 			const badge = await Badge.create(channel);
 			const sevenTV = new SevenTV();
 
-			const badges = await badge.fetchBadges();
-			const emoteData = await emote.fetchEmotes();
-			const stvGlobalEmotes = await sevenTV.fetchGlobalSTVEmotes();
+			try {
+				const badges = await badge.fetchBadges();
+				const emoteData = await emote.fetchEmotes();
+				const stvGlobalEmotes = await sevenTV.fetchGlobalSTVEmotes();
+				const stvChannelEmotes = await sevenTV.getSTVChannelEmotes(
+					channel
+				);
 
-			Badge.addCustomBadge(
-				'prodbyeagle',
-				'https://cdn.7tv.app/emote/01HY96D5S00005W8H9YKVEMA13/4x.avif'
-			);
+				Badge.addCustomBadge(
+					'prodbyeagle',
+					'https://cdn.7tv.app/emote/01H5ET82KR000B4C7M34K6ZCTK/4x.avif'
+				);
 
-			setSTVGlobalEmotes(stvGlobalEmotes);
-			setBadges(badges);
-			setEmotes(emoteData);
+				Badge.addCustomBadge(
+					'cronxz_tv',
+					'https://cdn.7tv.app/emote/01GRKSG9E000091TEC1A28HSM5/4x.webp'
+				);
 
-			chat.connect();
-			chat.onMessage((chatMessage) => {
-				handleCommand(chatMessage.message);
+				setSTVGlobalEmotes(stvGlobalEmotes);
+				setSTVChannelEmotes(stvChannelEmotes);
+				setBadges(badges);
+				setEmotes(emoteData);
 
-				setMessages((prev) => {
-					const updatedMessages = [
-						...prev,
-						{
-							username: chatMessage.username,
-							displayName: chatMessage.displayName,
-							message: chatMessage.message,
-							color: chatMessage.color,
-							badges: chatMessage.badges,
-						},
-					];
-					return updatedMessages.slice(-MAX_MESSAGES);
+				chat.connect();
+				chat.onMessage((chatMessage) => {
+					handleCommand(chatMessage.message);
+
+					setMessages((prev) => {
+						const updatedMessages = [
+							...prev,
+							{
+								username: chatMessage.username,
+								displayName: chatMessage.displayName,
+								message: chatMessage.message,
+								color: chatMessage.color,
+								badges: chatMessage.badges,
+							},
+						];
+						return updatedMessages.slice(-MAX_MESSAGES);
+					});
 				});
-			});
+			} catch (error) {
+				console.error('Fehler beim Abrufen der Daten:', error);
+			}
 
 			return () => {
 				chat.disconnect();
@@ -78,79 +97,18 @@ export default function ChatPage() {
 		fetchData();
 	}, [channel]);
 
-	const escapeRegExp = (string: string) => {
-		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-	};
-
-	const replaceEmotes = (message: string): (string | JSX.Element)[] => {
-		const parts: (string | JSX.Element)[] = [];
-		let lastIndex = 0;
-
-		const allEmotes = [
-			...(emotes ? [...emotes.global, ...emotes.channel] : []),
-			...(stvGlobalEmotes || []),
-		];
-
-		const regex = new RegExp(
-			`\\b(${allEmotes
-				.map((emote) => escapeRegExp(emote.name))
-				.join('|')})\\b`,
-			'g'
-		);
-
-		let match: RegExpExecArray | null;
-		while ((match = regex.exec(message)) !== null) {
-			parts.push(message.substring(lastIndex, match.index));
-
-			const emote = allEmotes.find((e) => e.name === match[0]);
-
-			if (emote) {
-				let imageUrl: string | null = null;
-
-				// Prüfe, ob es sich um ein 7TV-Emote handelt
-				if ('data' in emote && emote.data?.host?.url) {
-					imageUrl = `https:${emote.data.host.url}/4x.webp`;
-				} else if ('id' in emote) {
-					// Nutze die getEmoteUrl-Methode für Twitch-Emotes
-					imageUrl = new Emote(emote.broadcaster_id).getEmoteUrl(
-						emote.id,
-						'static', // statisch oder animiert
-						'3.0', // Skalierung
-						'dark' // Thema
-					);
-				}
-
-				if (imageUrl) {
-					parts.push(
-						<Image
-							key={match.index}
-							src={imageUrl}
-							alt={emote.name}
-							width={14}
-							height={16}
-							className='inline-block align-middle'
-						/>
-					);
-				}
-			}
-
-			lastIndex = regex.lastIndex;
-		}
-		parts.push(message.substring(lastIndex));
-		return parts;
-	};
-
 	useEffect(() => {
 		messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
 	}, [messages]);
 
 	return (
-		<div className='h-screen text-sm bg-neutral-900 text-neutral-100 p-4 overflow-hidden flex flex-col justify-end'>
+		//? BG ONLY FOR DEVELOPING (better to see the chat.)
+		<div className='h-screen text-xl bg-neutral-900 text-neutral-100 p-4 overflow-hidden flex flex-col justify-end cursor-default select-none'>
 			<div>
 				{messages.map((msg, index) => (
 					<div
 						key={index}
-						className='mb-1 flex items-center space-x-1'>
+						className='my-1 flex items-center space-x-1'>
 						{badges &&
 							Object.entries(msg.badges || {}).map(
 								([badge, version]) => {
@@ -165,8 +123,8 @@ export default function ChatPage() {
 										<Image
 											key={badge}
 											src={badgeUrl}
-											width={15}
-											height={15}
+											width={20}
+											height={20}
 											alt={badge}
 											unoptimized
 											className='rounded object-contain'
@@ -178,21 +136,29 @@ export default function ChatPage() {
 						{Badge.getCustomBadge(msg.username) && (
 							<Image
 								src={Badge.getCustomBadge(msg.username)!}
-								width={15}
-								height={15}
+								width={20}
+								height={20}
 								alt='Custom Badge'
 								unoptimized
 								className='rounded object-contain'
 							/>
 						)}
-						<span
-							className='font-bold'
-							style={{ color: msg.color }}>
-							{msg.displayName}
-						</span>
-						<span className='-ml-1'>:</span>
+						{msg.color && (
+							<span
+								className='font-semibold'
+								style={{
+									color: adjustColorBrightness(msg.color),
+								}}>
+								{msg.displayName}
+							</span>
+						)}
 						<span className='inline-flex flex-wrap'>
-							{replaceEmotes(msg.message)}
+							{replaceEmotes(
+								msg.message,
+								emotes,
+								STVGlobalEmotes,
+								STVChannelEmotes
+							)}
 						</span>
 					</div>
 				))}

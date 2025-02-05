fe3fe6ea562a340977b7089550e867827c713373
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-05 21:29:23 +0100

    feat: add environment variables for Twitch API and configure image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	env: {
+		TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
+		TWITCH_TOKEN: process.env.TWITCH_TOKEN,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'static-cdn.jtvnw.net',
+				pathname: '/badges/v1/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'static-cdn.jtvnw.net',
+				pathname: '/emoticons/v2/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
